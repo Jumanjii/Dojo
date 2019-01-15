e8f885295f3778c931e9b0ba200550fd8782672d
Allan Jacquet-Cretides <dragonjacquet@gmail.com>
2019-01-15 02:04:36 +0100

    init dailies resource

---
+++ migrations/schema.sql
@@ -0,0 +1,67 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 11.1 (Debian 11.1-1.pgdg90+1)
+-- Dumped by pg_dump version 11.1
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: dailies; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.dailies (
+    id uuid NOT NULL,
+    created_at timestamp without time zone NOT NULL,
+    updated_at timestamp without time zone NOT NULL,
+    reset_at timestamp without time zone NOT NULL,
+    recurrence integer NOT NULL,
+    name character varying(255) NOT NULL
+);
+
+
+ALTER TABLE public.dailies OWNER TO postgres;
+
+--
+-- Name: schema_migration; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE public.schema_migration (
+    version character varying(14) NOT NULL
+);
+
+
+ALTER TABLE public.schema_migration OWNER TO postgres;
+
+--
+-- Name: dailies dailies_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY public.dailies
+    ADD CONSTRAINT dailies_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: schema_migration_version_idx; Type: INDEX; Schema: public; Owner: postgres
+--
+
+CREATE UNIQUE INDEX schema_migration_version_idx ON public.schema_migration USING btree (version);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
---
+++ models/daily_test.go
@@ -0,0 +1,7 @@
+package models
+
+import "testing"
+
+func Test_Daily(t *testing.T) {
+	t.Fatal("This test needs to be implemented!")
+}
